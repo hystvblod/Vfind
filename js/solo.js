8a243c5db7ee3a1a584da8ce597aa35f5601ba36
hystvblod <vboldcompany@gmail.com>
2025-05-07 12:52:41 +0200

    d

--- js/solo.js
+++ js/solo.js
@@ -183,10 +183,5 @@ function afficherPhotosSauvegardees() {
       img.className = "photo-miniature";
       defiEl.appendChild(img);
     }
-    
   });
-  function init() {
-    document.getElementById("startBtn").addEventListener("click", startGame);
-    document.getElementById("replayBtn").addEventListener("click", resetGame);
-  }
 }
