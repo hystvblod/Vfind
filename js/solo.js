b6645135f6f33cebd73876fdf86648e720178641
hystvblod <vboldcompany@gmail.com>
2025-05-11 20:27:42 +0200

    s

--- js/solo.js
+++ js/solo.js
@@ -166,7 +166,20 @@ function afficherPhotosSauvegardees() {
       const photo = document.createElement("img");
       photo.className = "photo-user";
       photo.src = dataUrl;
-      photo.onclick = () => agrandirPhoto(dataUrl, id);
+      photo.onclick = () => {
+        const isPremium = getUserData().premium === true;
+      
+        if (isPremium) {
+          const confirmChange = confirm("Souhaites-tu supprimer cette photo et en prendre une autre ?");
+          if (confirmChange) {
+            localStorage.removeItem(`photo_defi_${id}`);
+            location.reload();
+          }
+        } else {
+          alert("❌ Seuls les utilisateurs premium peuvent changer leur photo.");
+        }
+      };
+      
 
       preview.appendChild(fond);
       preview.appendChild(photo);
