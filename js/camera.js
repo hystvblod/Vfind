0b6fd65764bb95c1f6ac9214b9c75ad45748ff54
hystvblod <vboldcompany@gmail.com>
2025-06-05 13:02:42 +0200

    1

--- js/camera.js
+++ js/camera.js
@@ -132,7 +132,8 @@ if (mode === "duel") {
   }
 
   const cadreImg = new Image();
-  cadreImg.src = `./assets/cadres/polaroid_01.webp`;
+  cadreImg.src = `./assets/cadres/polaroid_01.webp`;const cadreId = await getCadreSelectionne(); // vient de userData.js
+cadreImg.src = `./assets/cadres/${cadreId}.webp`;
 
   cadreImg.onload = async () => {
     // ✅ D'abord le cadre en fond
